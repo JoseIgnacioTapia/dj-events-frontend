57062559a3291f9ab861aa237440cbacc30e07a8
JoseIgnacioTapia <josignaciot@gmail.com>
2022-01-11 17:56:07 -0300

    DIsplaying Events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,28 @@
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
 
-function EventsPage() {
+export default function EventPage({ events }) {
   return (
-    <Layout title="">
-      <h1>My Events</h1>
+    <Layout>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events To Show</h3>}
+
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
 }
 
-export default EventsPage;
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 1,
+  };
+}
